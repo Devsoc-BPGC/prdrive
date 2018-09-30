3b6b1c378039c897f90a091459a1dff99da74c7c
Harsh Raj <f20170942@goa.bits-pilani.ac.in>
2018-09-30 23:02:42 +0530

    Login Activity codacy issues resolved

--- app/src/main/java/com/macbitsgoa/prdrive/adapters/HomeAdapter.java
+++ app/src/main/java/com/macbitsgoa/prdrive/adapters/HomeAdapter.java
@@ -13,7 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeViewHolder> {
 
-    SharedPreferences sharedPreferences;
+
     private String hostelNames[] = {"AH1", "AH2", "AH3", "AH4", "AH5", "AH6", "AH7",
             "AH8", "AH9", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "DH1", "DH2"};
 
--- app/src/main/java/com/macbitsgoa/prdrive/viewholders/HomeViewHolder.java
+++ app/src/main/java/com/macbitsgoa/prdrive/viewholders/HomeViewHolder.java
@@ -3,7 +3,6 @@ package com.macbitsgoa.prdrive.viewholders;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
