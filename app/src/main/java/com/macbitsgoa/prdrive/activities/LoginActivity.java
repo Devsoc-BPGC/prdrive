207d88d734940e22e44c794f74e36c5e19aaddd6
2017A7PS0927G <f20170927@goa.bits-pilani.ac.in>
2018-10-17 13:42:50 +0530

    done

--- app/src/main/java/com/macbitsgoa/prdrive/activities/LoginActivity.java
+++ app/src/main/java/com/macbitsgoa/prdrive/activities/LoginActivity.java
@@ -78,11 +78,15 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
             }
+
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
 
-     });
+        });
+        
     }
 }
+
